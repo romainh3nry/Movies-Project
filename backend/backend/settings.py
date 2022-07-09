53ebe450152fa097622f2cfb85eb3eb6e5084ae2
Romain HENRY <romainhenry60@gmail.com>
2022-07-09 10:57:20 +0200

    preparing directory frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -148,5 +148,5 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 20
 }
