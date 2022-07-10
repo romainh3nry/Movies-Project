76f7beb754163821cef308c3655a053f95fda41b
Romain HENRY <romainhenry60@gmail.com>
2022-07-10 08:29:39 +0200

    build sidebar

--- frontend/src/code/App.js
+++ frontend/src/code/App.js
@@ -1,8 +1,18 @@
+import {createGlobalStyle} from 'styled-components';
+import SideBar from './SideBar';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: linear-gradient(147deg, #000000 0%, #04619f 74%) no-repeat center center fixed; 
+    background-color: #000000;
+  }
+`
 
 const App = () => {
   return (
     <div>
-        <h3>Hello World</h3>
+        <GlobalStyle />
+        <SideBar />
     </div>
   )  
 }
