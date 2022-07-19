3012a4c8a80c2e9228753d46acdb2424ebaa55c0
Romain HENRY <romainhenry60@gmail.com>
2022-07-20 01:14:51 +0200

    handle next and previous movies listing

--- frontend/src/code/Movies/index.js
+++ frontend/src/code/Movies/index.js
@@ -2,7 +2,17 @@ import styled from 'styled-components';
 import {Row, Col, Container} from 'react-bootstrap';
 import React from "react";
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
+
+const getUrl = (page) => (
+    `http://127.0.0.1:8000/api/v1/movies/all/?page=${page}`
+)
+
+const extractPage = (url) => {
+    const page = url.split('=')
+    return parseInt(page[1])
+}
 
 const moviesListReducer = (action, state) => {
     switch (action.type) {
@@ -30,25 +40,56 @@ const moviesListReducer = (action, state) => {
     }
 }
 
+
 const Movies = () => {
 
-    const handleFetchMovies = (page) => {
-        dispatchMovies({type: 'MOVIES_FETCH_INIT'})
-        axios
-            .get(`http://127.0.0.1:8000/api/v1/movies/all/?page=${page}`)
-    }
-
-    const [movies, dispatchMovies] = React.useReducer(
+    const [movie, dispatchMovie] = React.useReducer(
         moviesListReducer,
         {data: [], isLoading: false, isError: false}
     )
+    const [urls, setUrls] = React.useState(getUrl(1))
+
+    const handleFetchMovies = React.useCallback(() => {
+        dispatchMovie({type: 'MOVIES_FETCH_INIT'})
+        axios
+            .get(urls)
+            .then(result => {
+                dispatchMovie({
+                    type: 'MOVIES_FETCH_SUCCESS',
+                    payload: result.data,
+                })
+            })
+            .catch(() => {
+                dispatchMovie({type: 'MOVIES_FETCH_FAILURE'})
+            })
+    }, [urls])
 
     React.useEffect(() => {
-        handleFetchMovies(1)
-    }, [])
+        handleFetchMovies()
+    }, [handleFetchMovies])
+
+    const handleDisplay = (page) => {
+        const url = getUrl(page)
+        setUrls(url);
+    }
+
+    const handleNext = () => {
+        const page = extractPage(urls)
+        handleDisplay(page + 1)
+    }
+
+    const handlePrevious = () => {
+        const page = extractPage(urls)
+        handleDisplay(page - 1)
+    }
 
     return (
+        <>
         'all movies'
+        <button onClick={handlePrevious} type="button">Previous</button>
+        <button onClick={handleNext} type="button">Next</button>
+        </>
+        
     )
 }
 
