348f1c0d14805c8abce07ef232d51da7208a0d06
Romain HENRY <romainhenry60@gmail.com>
2022-07-19 23:41:24 +0200

    add reducer for dispatching movies

--- frontend/src/code/Movies/index.js
+++ frontend/src/code/Movies/index.js
@@ -3,7 +3,39 @@ import {Row, Col} from 'react-bootstrap';
 import React from "react";
 import axios from 'axios';
 
+const moviesListReducer = (action, state) => {
+    switch (action.type) {
+        case 'MOVIES_FETCH_INIT':
+            return {
+                ...state,
+                isLoading: true,
+                isError: false
+            }
+        case 'MOVIES_FETCH_SUCCESS':
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data: action.payload
+            }
+        case 'MOVIES_FETCH_FAILURE':
+            return {
+                ...state,
+                isLoading: false,
+                isError: true
+            }
+        default:
+            return state
+    }
+}
+
 const Movies = () => {
+
+    const [movies, moviesDispatch] = React.useReducer(
+        moviesListReducer,
+        {data: [], isLoading: false, isError: false}
+    )
+
     return (
         'all movies'
     )
